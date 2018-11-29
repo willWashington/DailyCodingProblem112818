d5ecdad8ce2909d91fbc0f59d633904ad2ca76fe
w2 <william.washington@live.com>
2018-11-28 22:10:15 -0500

    Working with Linq Solution

--- DailyCodingProblem112818/Program.cs
+++ DailyCodingProblem112818/Program.cs
@@ -18,25 +18,31 @@ namespace DailyCodingProblem112818
                 You can modify the input array in-place. 
              */
 
-            var arr = new int[] { 3, 4, -1, 1 };
-            var result = new List<int>();
-            int answer = 1;
+            var arr = new int[] { 1,2,0};
+            List<int> answerList = new List<int>();
 
             for (int i = 0; i < arr.Length; i++)
-            { //for every int in the array
-                foreach (int num in arr) 
-                { //compare it to every other int in the array
-
-                    int comparison = num + 1;
-                    int max = arr.Max();
-                    if (comparison > 0 && comparison > i && !arr.Contains(comparison) && comparison < max)                         
-                    {
-                        Debug.WriteLine(comparison);
-                    }
-                    //returns 2 2 - almost there
+            { //for every int in the array                
+                int min = arr.Min(); //get the lowest number in the array
+                int minIndex = Array.IndexOf(arr, min); //find what index position the lowest number in the array is
+                int max = arr.Max(); //get the highest number in the array
+                int comparison = arr[i] + 1; //add 1 to the current array selection
+                if (comparison > 0 && comparison > arr[i] && !arr.Contains(comparison))
+                    //if comparison is:
+                        //positive (greater than 0) AND
+                        //greater than the current number in the outer loop AND
+                        //is not in the original array AND
+                        //is less than the highest number in the array
+                {
+                    answerList.Add(comparison);                    
+                } else
+                {
+                    Debug.WriteLine("incorrect algorithm");
                 }
             }
-            Console.ReadLine();
+            //linq solution:
+            int finalAnswer = answerList.Min();
+            Debug.WriteLine(finalAnswer); //write it
         }
     }
 }
