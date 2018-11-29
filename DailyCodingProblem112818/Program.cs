e550f6ad4ddaf8878b9e52b4a0e4829d399a1c65
w2 <william.washington@live.com>
2018-11-28 22:47:40 -0500

    Finalized Long Method - Possible Short Method if Next Iteration

--- DailyCodingProblem112818/Program.cs
+++ DailyCodingProblem112818/Program.cs
@@ -18,29 +18,32 @@ namespace DailyCodingProblem112818
                 You can modify the input array in-place. 
              */
 
-            var arr = new int[] { 1,2,0};
+            var arr = new int[] { };
             List<int> answerList = new List<int>();
 
             for (int i = 0; i < arr.Length; i++)
-            { //for every int in the array                
+            { //for every int in the array
                 int min = arr.Min(); //get the lowest number in the array
                 int minIndex = Array.IndexOf(arr, min); //find what index position the lowest number in the array is
                 int max = arr.Max(); //get the highest number in the array
-                int comparison = arr[i] + 1; //add 1 to the current array selection
-                if (comparison > 0 && comparison > arr[i] && !arr.Contains(comparison))
-                    //if comparison is:
+                int possibleAnswer = arr[i] + 1; //add 1 to the current array selection
+                if (possibleAnswer > 0 && !arr.Contains(possibleAnswer))
+                    //if possibleAnswer is:
                         //positive (greater than 0) AND
-                        //greater than the current number in the outer loop AND
-                        //is not in the original array AND
-                        //is less than the highest number in the array
+                        //is not in the original array
                 {
-                    answerList.Add(comparison);                    
+                    answerList.Add(possibleAnswer);
                 } else
                 {
                     Debug.WriteLine("incorrect algorithm");
                 }
             }
             //linq solution:
+
+            if (answerList.Count == 0 || answerList.Aggregate((a, x) => a * x) < 0)
+            {
+                answerList.Add(1);
+            }
             int finalAnswer = answerList.Min();
             Debug.WriteLine(finalAnswer); //write it
         }
